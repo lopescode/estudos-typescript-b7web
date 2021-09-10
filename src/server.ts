0cc021e97526028a7f2510fec312fa6c46f5fbcd
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-09-10 00:02:01 -0300

    Importando arquivos de terceiros

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@
 /************************************ */
 /** Entendendo import/exports em ES6 */
 /********************************** */
-
+/*
 //import { somar, subtrair, multiplicar} from './matematica';
 //import * as Matematica from './matematica';
 import Matematica from "./matematica";
@@ -18,3 +18,14 @@ let n2: number = 10;
 console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
 console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
 console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
+*/
+
+/************************************ */
+/** Importando arquivos de terceiros */
+/********************************** */
+
+import validator from "validator";
+
+console.log(validator.isEmail("suporte@krolik.com.br"));
+console.log(validator.isIP("127.0.0.1"));
+console.log(validator.isLowercase("Leonardo"));
