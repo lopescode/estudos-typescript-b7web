79ae51dd6e7333502c25d2c308cc946a2d23b4f0
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-09-11 00:13:48 -0300

    Mustache: Exibindo listas (loop)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,20 +9,35 @@ router.get("/", (req: Request, res: Response) => {
   /** Mustache: Exibindo informações */
   let user = {
     name: "Leonardo",
-    age: 24
-  }
-  let showOld: boolean = false;
+    age: 24,
+  };
 
-  if(user.age > 18) {
+  /** Mustache: Exibição condicional */
+  let showOld: boolean = false;
+  if (user.age > 18) {
     showOld = true;
   }
 
-  res.render('home', {
+  res.render("home", {
+    /** Mustache: Exibindo informações */
     user,
+
     /** Mustache: Exibição condicional */
     //showWelcome: false // Não exibe
     showWelcome: true, // Exibe
-    showOld
+    showOld,
+
+    /** Mustache: Exibindo listas (loop) */
+    products: [
+      { title: "Produto X", price: 10 },
+      { title: "Produto Y", price: 15 },
+      { title: "Produto W", price: 20 }
+    ],
+    // ListaS simples
+    frasesDoDia: [
+      "Alguma coisa muito legal",
+      "Outra frase qualquer"
+    ]
   });
 });
 
