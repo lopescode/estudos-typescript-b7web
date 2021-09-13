99c8d296364503efd1f471cc10909c1bf4dce310
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-09-12 22:26:14 -0300

    Trabalhando com Models 2

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 
+/** Trabalhando com Models 2 */
+import { Product } from "../models/Product";
+
 export const home = (req: Request, res: Response) => {
   /** Mustache: Exibindo informações */
   let user = {
@@ -9,9 +12,14 @@ export const home = (req: Request, res: Response) => {
 
   /** Mustache: Exibição condicional */
   let showOld: boolean = false;
+
   if (user.age < 18) {
     showOld = true;
   }
+
+  /** Trabalhando com Models 2 */
+  let list = Product.getAll();
+  let expensiveList = Product.getFromPriceAfter(12);
 
   res.render("pages/home", {
     /** Mustache: Exibindo informações */
@@ -22,12 +30,17 @@ export const home = (req: Request, res: Response) => {
     showWelcome: true, // Exibe
     showOld,
 
-    /** Mustache: Exibindo listas (loop) */
+    /** Mustache: Exibindo listas (loop) 
     products: [
       { title: "Produto X", price: 10 },
       { title: "Produto Y", price: 15 },
       { title: "Produto W", price: 20 },
     ],
+    */
+
+    /** Trabalhando com Models 2 */
+    products: list,
+    expensives: expensiveList,
 
     // Listas simples
     /*
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -8,10 +8,16 @@ const data: Product[] = [
   { title: "Produto X", price: 10 },
   { title: "Produto Y", price: 15 },
   { title: "Produto W", price: 20 },
+  { title: "Produto Z", price: 25 },
 ];
 
 export const Product = {
   getAll: (): Product[] => {
     return data;
+  },
+
+  /** Trabalhando com Models 2 */
+  getFromPriceAfter: (price: number): Product[] => {
+    return data.filter((item) => item.price >= price);
   },
 };
