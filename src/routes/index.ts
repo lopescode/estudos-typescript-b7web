a1110ba115b1a9c0c34d32300fff765c262346a9
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-09-12 21:13:40 -0300

    Recebendo dados na URL 1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -58,4 +58,18 @@ router.get("/sobre", (req: Request, res: Response) => {
   res.render('sobre')
 });
 
+
+router.get("/nome", (req: Request, res: Response) => {
+  
+  /** Recebendo dados na URL 1 */
+  console.log(req.query)
+
+  let nome: String = req.query.nome as string; // Especificando que o retorno será string
+
+  res.render('nome', {
+    nome
+  })
+});
+
+
 export default router;
