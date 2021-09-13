306773b2552e2eceef1ead94eef2b5aceccd009d
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-09-12 22:15:57 -0300

    Trabalhando com Models 1

---
+++ src/models/Product.ts
@@ -0,0 +1,17 @@
+/** Trabalhando com Models 1 */
+type Product = {
+  title: string;
+  price: number;
+};
+
+const data: Product[] = [
+  { title: "Produto X", price: 10 },
+  { title: "Produto Y", price: 15 },
+  { title: "Produto W", price: 20 },
+];
+
+export const Product = {
+  getAll: (): Product[] => {
+    return data;
+  },
+};
